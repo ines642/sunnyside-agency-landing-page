153e0ac285472538c6f81794a95c42dfff7d8631
ines642 <ineschicha4@gmail.com>
2022-09-27 01:59:43 +0100

    fixe navbar problem and adding simple animation

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ const creative=document.querySelector('.creative')
 menu.addEventListener('mouseover' ,()=>{
     navbar.style.display='flex'
     creative.style.display='none'
-    menu.style.transition='2s'
+    menu.style.transition='0.5s'
     menu.style.opacity=0.6
 })
-menu.addEventListener('mouseout' ,()=>{
+//mouse out event is fired when mmouse leave the parent or one of it children
+//mouseleave event is fired when mmouse leave the parent onely
+navbar.addEventListener('mouseleave' ,()=>{
     navbar.style.display='none'
     creative.style.display='block'
-    menu.style.transition='2s'
+    menu.style.transition='0.5s'
 
 })
